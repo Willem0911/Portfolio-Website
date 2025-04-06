aa0a533d89f05c02ef7d1a103b976d1d60b230df
[Willem Erasmus] <[ewillem84@gmail.com]>
2025-04-06 18:07:48 +0200

    Working Navbar

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function showMobileNav() {
+    let mobileNav = document.getElementById("mobileNav");
+    mobileNav.style.width = "100vw";
+    
+}
+function hideMobileNav() {
+    let mobileNav = document.getElementById("mobileNav");
+    mobileNav.style.width = "0vw";
+}
