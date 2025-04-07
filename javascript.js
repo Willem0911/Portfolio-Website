bac6ac304ca5b34951f43ac2acbe641f694182fa
[Willem Erasmus] <[ewillem84@gmail.com]>
2025-04-07 22:05:01 +0200

    Add the hero

--- javascript.js
+++ javascript.js
@@ -7,3 +7,29 @@ function hideMobileNav() {
     let mobileNav = document.getElementById("mobileNav");
     mobileNav.style.width = "0vw";
 }
+let text = document.getElementById("fullName");
+let fullText = text.innerHTML;
+let lenText = fullText.length;
+text.innerHTML = "";
+let endText = ""
+console.log(lenText);
+let i = 0;
+const myFunction = (i) => {
+    setTimeout(() => {
+        endText += fullText[i];
+        text.innerHTML = endText + "|";
+        console.log({i});
+        i++;
+
+        if (i < lenText){
+            
+            myFunction(i);
+        }
+        else {
+            endText = endText.slice(0, -1);
+            text.innerHTML = endText;
+        }
+    },75);
+};
+myFunction(i);
+console.log(text);
